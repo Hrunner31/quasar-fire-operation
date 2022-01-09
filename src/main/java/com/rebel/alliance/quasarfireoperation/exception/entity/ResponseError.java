619349aa4e889e00b9159584c2d490744148b022
H. Leonardo A. Corredor <hleonard31@gmail.com>
2022-01-09 12:14:27 -0500

    Configure entity ResponseError

--- src/main/java/com/rebel/alliance/quasarfireoperation/exception/entity/ResponseError.java
+++ src/main/java/com/rebel/alliance/quasarfireoperation/exception/entity/ResponseError.java
@@ -1,8 +1,11 @@
 package com.rebel.alliance.quasarfireoperation.exception.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class ResponseError {
-
+	private int code;
+	private String message;
 }
