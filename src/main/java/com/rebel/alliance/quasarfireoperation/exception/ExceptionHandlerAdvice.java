32850f13c10446f94453777ee4cc8755b04cd0ac
H. Leonardo A. Corredor <hleonard31@gmail.com>
2022-01-09 23:39:34 -0500

    Configure ExceptionHandlerAdvice to manage the logs

--- src/main/java/com/rebel/alliance/quasarfireoperation/exception/ExceptionHandlerAdvice.java
+++ src/main/java/com/rebel/alliance/quasarfireoperation/exception/ExceptionHandlerAdvice.java
@@ -1,7 +1,8 @@
 package com.rebel.alliance.quasarfireoperation.exception;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
@@ -9,30 +10,41 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import com.rebel.alliance.quasarfireoperation.exception.entity.ResponseError;
-import com.rebel.alliance.qusarfireoperation.utilities.Constant;
-import com.rebel.alliance.qusarfireoperation.utilities.Utility;
+import com.rebel.alliance.quasarfireoperation.utilities.Constant;
+import com.rebel.alliance.quasarfireoperation.utilities.Utility;
 
 @ControllerAdvice
 public class ExceptionHandlerAdvice {
 	
-	private static final Logger logger = LogManager.getLogger(ExceptionHandlerAdvice.class);
+	private static Logger logger = LoggerFactory.getLogger(ExceptionHandlerAdvice.class);
+	
+	Utility utilities;
+	
+	@Autowired
+	public ExceptionHandlerAdvice(Utility utilities) {
+		this.utilities = utilities;
+	}
 	
 	@ExceptionHandler(Exception.class)
+	@ResponseBody
 	public ResponseError handleUnexpectedError(Exception exception) {
-		logger.error(Constant.UNEXPECTED_ERROR, Utility.getStackError(exception));
+		logger.error(Constant.UNEXPECTED_ERROR, this.utilities.getStackError(exception));
 		String message = exception.getMessage() != null ? exception.getMessage() : Constant.UNEXPECTED_ERROR;
 		return new ResponseError(HttpStatus.INTERNAL_SERVER_ERROR.value(), message);
 	}
 	
 	@ExceptionHandler({
 		LocationException.class, 
-		MessageException.class
+		MessageException.class,
+		SatelliteException.class
 	})
+	@ResponseBody
 	public ResponseError handleBusinessError(Exception exception) {
-		logger.error(Constant.INDETERMINATE_ERROR, Utility.getStackError(exception));
+		logger.error(Constant.INDETERMINATE_ERROR, this.utilities.getStackError(exception));
 		String message = exception.getMessage() != null ? exception.getMessage() : Constant.INDETERMINATE_ERROR;
 		return new ResponseError(HttpStatus.BAD_REQUEST.value(), message);
 	}
@@ -44,8 +56,9 @@ public class ExceptionHandlerAdvice {
 		MethodArgumentTypeMismatchException.class,
 		HttpMessageNotReadableException.class
 	})
+	@ResponseBody
 	public ResponseError handleBadRequest(Exception exception) {
-		logger.error(Constant.INVALID_REQUEST, Utility.getStackError(exception));
+		logger.error(Constant.INVALID_REQUEST, this.utilities.getStackError(exception));
 		String message = exception.getMessage() != null ? exception.getMessage() : Constant.INVALID_REQUEST;
 		return new ResponseError(HttpStatus.BAD_REQUEST.value(), message);
 	}
